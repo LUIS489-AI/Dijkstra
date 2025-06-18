98e13106dd5c19687bfe3835d91dc60d550e93e7
LUIS489-AI <luisalanyav@gmail.com>
2025-06-18 13:35:51 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'airport-calculator-v1';
+const urlsToCache = [
+    './simulador.html' 
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache abierto');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
